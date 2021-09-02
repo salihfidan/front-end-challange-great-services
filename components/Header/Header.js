d014bcd9361e02081e892b8d91c6f06801e05a2a
Hamza Fidan <hf1d4n@gmail.com>
2021-09-02 21:00:59 +0300

    linting fix, scss and components

---
+++ components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// component styling
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      Header
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// component styling
+import styles from './Layout.module.scss';
+
+const Layout = () => {
+  return (
+    <div className={styles.layout}>
+      Layout
+    </div>
+  );
+};
+
+export default Layout;
