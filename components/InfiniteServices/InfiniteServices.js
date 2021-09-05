8a7305fafcac96ea3a0a4d872d8f3391eb348430
Hamza Fidan <hf1d4n@gmail.com>
2021-09-05 22:51:33 +0300

    features

--- components/InfiniteServices/InfiniteServices.js
+++ components/InfiniteServices/InfiniteServices.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import useSWRInfinite from 'swr/infinite';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+// import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 
 // helpers
 import fetcher from '../../helpers/fetcher';
 
 // hooks
-import useWindowDimensions from '../../hooks/useWindowDimensions';
+// import useWindowDimensions from '../../hooks/useWindowDimensions';
 import useIntersection from '../../hooks/useIntersection';
 
 // components
@@ -14,7 +14,7 @@ import ServiceCard from '../ServiceCard';
 
 const InfiniteServices = () => {
   const viewMoreButtonRef = useRef();
-  const inViewport = useIntersection(viewMoreButtonRef);
+  // const inViewport = useIntersection(viewMoreButtonRef);
   const PAGE_SIZE = 12;
   const { data, error, size, setSize } = useSWRInfinite((index) => `api/services/${index * PAGE_SIZE}/${PAGE_SIZE}`, fetcher);
 
@@ -24,36 +24,33 @@ const InfiniteServices = () => {
   const isEmpty = data?.[0]?.length === 0;
   const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
 
-  useEffect(() => {
-    if (inViewport && !isLoadingMore && !isReachingEnd) setSize(size + 1);
-  }, [inViewport, isLoadingMore, isReachingEnd]);
+  // useEffect(() => {
+  //   if (inViewport && !isLoadingMore && !isReachingEnd) setSize(size + 1);
+  // }, [inViewport, isLoadingMore, isReachingEnd]);
 
   return (
-    <div>
-      <div className="container-xxl">
-        <div className="row mt-lg-6">
-          {services.map((item, i) => {
-            const { title, icon, people, category } = item;
-            return (
-              <div className="col-6 col-md-4 col-lg-3 col-xxl-2 mt-6 mt-md-9" key={i + item.title}>
-                <ServiceCard title={title} icon={icon} people={people} category={category} />
-              </div>
-            );
-          })}
-          {isLoadingMore
-            ? [...new Array(PAGE_SIZE)].map((_, i) => {
-                return (
-                  <div className="col-6 col-md-4 col-lg-3 col-xxl-2 mt-6 mt-md-9" key={i}>
-                    <ServiceCard isLoading={true} />
-                  </div>
-                );
-              })
-            : ''}
-        </div>
-        <div ref={viewMoreButtonRef}></div>
-        <div className="mb-5 pb-5"></div>
+    <>
+      <div className="row mt-lg-6 mb-10 mb-lg-11">
+        {services.map((item, i) => {
+          const { title, icon, people, category } = item;
+          return (
+            <div className="col-6 col-md-4 col-lg-3 col-xxl-2 mt-6 mt-md-9" key={i + item.title}>
+              <ServiceCard title={title} icon={icon} people={people} category={category} />
+            </div>
+          );
+        })}
+        {isLoadingMore
+          ? [...new Array(PAGE_SIZE)].map((_, i) => {
+              return (
+                <div className="col-6 col-md-4 col-lg-3 col-xxl-2 mt-6 mt-md-9" key={i}>
+                  <ServiceCard isLoading={true} />
+                </div>
+              );
+            })
+          : ''}
       </div>
-    </div>
+      <div ref={viewMoreButtonRef}></div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import InfiniteServices from '../components/InfiniteServices';
 
+// icons
+import { CheckIcon } from '../components/SvgIcon';
+
 // page styling
 import styles from '../styles/pages/Home.module.scss';
 
@@ -19,7 +22,62 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout pageTitle={pageTitle} pageDescription={pageDescription}>
-        <InfiniteServices />
+        <div className="container-xxl">
+          <InfiniteServices />
+        </div>
+        <div className={styles.features}>
+          <div className={styles['features__items']}>
+            <div className="container-xxl">
+              <div className="row">
+                <div className="col-sm-6 col-xl-3 mb-30 mb-sm-7 mb-xl-100">
+                  <div className={styles['features__item']}>
+                    <div className={styles['features__item-icon-area']}>
+                      <CheckIcon />
+                    </div>
+                    <div className={styles['features__item-text-area']}>
+                      <div className={styles['features__item-title']}>Manage your task</div>
+                      <div className={styles['features__item-text']}>No matter what the size of the task is we will manage it and find solutions for you. From cleaning to junk removal to fixing we have got you covered.</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-6 col-xl-3 mb-30 mb-sm-7 mb-xl-100">
+                  <div className={styles['features__item']}>
+                    <div className={styles['features__item-icon-area']}>
+                      <CheckIcon />
+                    </div>
+                    <div className={styles['features__item-text-area']}>
+                      <div className={styles['features__item-title']}>Define the scope of work</div>
+                      <div className={styles['features__item-text']}>We will have the subject-matter expert define the scope of work.</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-6 col-xl-3 mb-30 mb-sm-7 mb-xl-100">
+                  <div className={styles['features__item']}>
+                    <div className={styles['features__item-icon-area']}>
+                      <CheckIcon />
+                    </div>
+                    <div className={styles['features__item-text-area']}>
+                      <div className={styles['features__item-title']}>Vet licensed vendor</div>
+                      <div className={styles['features__item-text']}>We make sure they have a license, valid insurance, criminal background check, and are trusted in their local community.</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-6 col-xl-3 mb-30 mb-sm-7 mb-xl-100">
+                  <div className={styles['features__item']}>
+                    <div className={styles['features__item-icon-area']}>
+                      <CheckIcon />
+                    </div>
+                    <div className={styles['features__item-text-area']}>
+                      <div className={styles['features__item-title']}>3 Quotes, until you are satisfied.</div>
+                      <div className={styles['features__item-text']}>We will get 3 quotes, If not satisfied we will get more quotes at no cost.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles['features__email-area']}></div>
+          </div>
+        </div>
         {/* <div style={{ height: '101vh' }}></div> */}
       </Layout>
     </div>
