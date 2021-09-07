0b376e4adf4d85f98f90791c64abf8f24e478769
Hamza Fidan <hf1d4n@gmail.com>
2021-09-07 20:08:26 +0300

    added uniquekey to skeleton loaders

--- components/ServiceCard/ServiceCard.js
+++ components/ServiceCard/ServiceCard.js
@@ -5,7 +5,7 @@ import ContentLoader from 'react-content-loader';
 // component styling
 import styles from './ServiceCard.module.scss';
 
-const ServiceCard = ({ title, icon, people, category, isLoading }) => {
+const ServiceCard = ({ title, icon, people, category, isLoading, key }) => {
   const [_people, _setPeople] = useState([]);
 
   useEffect(() => {
@@ -21,36 +21,36 @@ const ServiceCard = ({ title, icon, people, category, isLoading }) => {
       <div className={styles['service-card']}>
         <div className={styles['service-card__img-container']}>
           <div className={styles['service-card__img']}>
-            <ContentLoader uniqueKey="icon" width={'100%'} height={'100%'}>
+            <ContentLoader uniqueKey={`icon${key}`} width={'100%'} height={'100%'}>
               <rect x="0" y="0" rx="50%" ry="50%" width="100%" height="100%" />
             </ContentLoader>
           </div>
         </div>
         <div className={styles['service-card__title']} style={{ textAlign: 'center' }}>
-          <ContentLoader uniqueKey="title" width={'100%'} height={16}>
+          <ContentLoader uniqueKey={`title${key}`} width={'100%'} height={16}>
             <rect x="5%" y="0" rx="6" ry="6" width="90%" height="100%" />
           </ContentLoader>
         </div>
         <div className={styles['service-card__expert-area']}>
           <div className={styles['service-card__expert-title']}>
-            <ContentLoader uniqueKey="expert-title" width={'100%'} height={14}>
+            <ContentLoader uniqueKey={`expert-title${key}`} width={'100%'} height={14}>
               <rect x="25%" y="0" rx="6" ry="6" width="50%" height="100%" />
             </ContentLoader>
           </div>
           <div className={styles['service-card__expert-images']}>
             <div className={styles['service-card__expert-img']}>
-              <ContentLoader uniqueKey="expert-img" width={'100%'} height={'100%'}>
+              <ContentLoader uniqueKey={`expert-img${key}`} width={'100%'} height={'100%'}>
                 <rect x="0" y="0" rx="50%" ry="50%" width="100%" height="100%" />
               </ContentLoader>
             </div>
             <div className={styles['service-card__expert-img']}>
-              <ContentLoader uniqueKey="expert-img-2" width={'100%'} height={'100%'}>
+              <ContentLoader uniqueKey={`expert-img-2${key}`} width={'100%'} height={'100%'}>
                 <rect x="0" y="0" rx="50%" ry="50%" width="100%" height="100%" />
               </ContentLoader>
             </div>
           </div>
           <div className={styles['service-card__expert-names']}>
-            <ContentLoader uniqueKey="expert-names" width={'100%'} height={14}>
+            <ContentLoader uniqueKey={`expert-names${key}`} width={'100%'} height={14}>
               <rect x="10%" y="0" rx="6" ry="6" width="80%" height="100%" />
             </ContentLoader>
           </div>
